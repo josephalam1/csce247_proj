c3bb8b282e0dbf98128076b3e68bae123bee6103
omg-chloe <oli.oli.oxen.free.odessa@gmail.com>
2021-10-18 06:22:29 -0400

    private to public

--- src/Features.java
+++ src/Features.java
@@ -3,6 +3,7 @@ package src;
 import java.util.ArrayList;
 
 // COS changed all to public for now because of errors
+// reviewed teachers interfaces all are public need to change UML
 
 public interface Features {
     public ArrayList<String> search();
