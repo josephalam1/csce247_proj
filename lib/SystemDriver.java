11f61f5f840adb15dd6aabccbe00d6ea9ba4f9e3
omg-chloe <oli.oli.oxen.free.odessa@gmail.com>
2021-10-17 23:17:42 -0400

    Driver test

---
+++ lib/SystemDriver.java
@@ -0,0 +1,13 @@
+package lib;
+
+public class SystemDriver {
+
+    public void run() {
+        System.out.println("x");
+    }
+
+    public static void main(String[] args) {
+        SystemDriver driver = new SystemDriver();
+        driver.run();
+    }
+}
