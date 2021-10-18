e33605a4152606bec871ddafd0320690fc787aec
Ben dela Pena <bdelapena120@gmail.com>
2021-10-18 00:29:42 -0400

    adding interface stubs

---
+++ Features.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public interface Features {
+    public ArrayList<String> search();
+    public ArrayList<String> sort(ArrayList[] search);
+    public ArrayList<String> filter(ArrayList[] search);
+    public double giveRating(CopanyProfile aProfile);
+}
---
+++ ProfileSettings.java
@@ -0,0 +1,6 @@
+public interface ProfileSettings {
+    public hideFromProfile(String aProfile)
+    private deleteProfile(String aProfile, Bool adminPrivilege);
+    private overrideRating (Bool adminPrivilege);
+
+}
