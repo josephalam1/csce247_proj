2c78a2bfa2d87c1bc78cee1ccd45c7d90594b328
Joey Alam <jaalam@email.sc.edu>
2021-10-28 14:19:54 -0400

    Changed dataloader and datawriter

--- src/DataWriter.java
+++ src/DataWriter.java
@@ -7,8 +7,26 @@ import org.json.simple.parser.JSONParser;
 import java.util.ArrayList;
 
 public class DataWriter {
-    public static void saveStudents(ArrayList<Student> students) {
+    public static void saveStudents() {
+        Students studentInst = Students.getInstance();
+        ArrayList<Student> students = studentInst.getStudents();
         JSONArray studentsJ = new JSONArray();
+        for (int i = 0; i < students.size(); i++) {
+            //studentsJ.add(students.get(i));
+        }
     }
-    //public static 
+    public static void saveCompanies() {
+
+    }
+    public static void saveResumes() {
+
+    }
+    public static void saveJobs() {
+
+    }
+
+    public static JSONObject getStudentsJSON(Student student) {
+        JSONObject studentDetails = new JSONObject();
+        return studentDetails;
+    }
 }
